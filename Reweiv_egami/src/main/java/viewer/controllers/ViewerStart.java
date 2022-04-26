aa6c34e2f5983d46f502c75152af87bceb0667a8
Kiszel Pál János <97761852+kiszpal@users.noreply.github.com>
2022-04-26 16:30:32 +0200

    FXML loading hiba
    valszeg nem találja a fájlt. KEzdeni kéne ezzel valamit, de megyek enni.

--- Reweiv_egami/src/main/java/viewer/controllers/ViewerStart.java
+++ Reweiv_egami/src/main/java/viewer/controllers/ViewerStart.java
@@ -7,14 +7,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class ViewerStart extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ViewerStart.class.getResource("viewer.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(ViewerStart.class.getResource("viewer.fxml"));
         //double width = Screen.getPrimary().getBounds().getWidth();
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        //Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane,1280,720);
         stage.setTitle("Reweiv egami");
         stage.setScene(scene);
         stage.show();
